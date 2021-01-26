08ea1a1a42fa7bd50887e9dca62b1babce53bbb9
liupeng <624298206@qq.com>
2021-01-26 20:17:54 +0800

    update

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -13,7 +13,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'), //打包后的目录，必须是绝对路径
     filename: 'js/[name].[hash:6].js', // 入口文件打包后的文件名称
     chunkFilename: 'js/[name].[hash:8].js', // 块打包后的文件名称
-    publicPath: '/', //通常是CDN地址
+    publicPath: '/' //通常是CDN地址
   },
   module: {
     // 从右向左解析
@@ -21,7 +21,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         use: ['cache-loader', 'babel-loader'],
-        include: [path.resolve(__dirname, 'src')],
+        include: [path.resolve(__dirname, 'src')]
       },
       // vue文件解析
       {
@@ -32,11 +32,11 @@ module.exports = {
             options: {
               compilerOptions: {
                 // 清除模板html标签之间的空格
-                preserveWhitespace: false,
-              },
-            },
-          },
-        ],
+                preserveWhitespace: false
+              }
+            }
+          }
+        ]
       },
       {
         test: /\.(le|c)ss$/,
@@ -45,21 +45,21 @@ module.exports = {
             loader: MiniCssExtractPlugin.loader,
             options: {
               hmr: isDev,
-              reloadAll: true,
-            },
+              reloadAll: true
+            }
           },
           'css-loader',
           {
             loader: 'postcss-loader',
             options: {
-              plugins: function() {
+              plugins: function () {
                 return [require('autoprefixer')()]
-              },
-            },
+              }
+            }
           },
-          'less-loader',
+          'less-loader'
         ],
-        include: [path.resolve(__dirname, 'src')],
+        include: [path.resolve(__dirname, 'src')]
       },
       {
         test: /\.(png|jpg|gif|jpeg|webp|svg|eot|ttf|woff|woff2)$/,
@@ -69,21 +69,21 @@ module.exports = {
             options: {
               limit: 10240, //10K
               esModule: false,
-              name: '[name]_[hash:6].[ext]',
-            },
-          },
+              name: '[name]_[hash:6].[ext]'
+            }
+          }
         ],
-        include: [path.resolve(__dirname, 'src')],
-      },
-    ],
+        include: [path.resolve(__dirname, 'src')]
+      }
+    ]
   },
   resolve: {
     // 别名方便引入
     alias: {
-      ' @': path.resolve(__dirname, '../src'),
+      ' @': path.resolve(__dirname, '../src')
     },
     // 自定义解析后缀，引入时不需要写文件后缀
-    extensions: ['.js', '.json', '.vue'],
+    extensions: ['.js', '.json', '.vue']
   },
   plugins: [
     new HardSourceWebpackPlugin(), // 提高构建速度
@@ -93,8 +93,8 @@ module.exports = {
       filename: 'index.html', //打包后的文件名
       minify: {
         removeAttributeQuotes: false, //是否删除属性的双引号
-        collapseWhitespace: false, //是否折叠空白
-      },
-    }),
-  ],
+        collapseWhitespace: false //是否折叠空白
+      }
+    })
+  ]
 }
